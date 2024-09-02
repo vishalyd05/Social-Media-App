4a7d6349570450de824d11fafba8fdb282c5c06a
vishalyd05 <vydhoni57@gmail.com>
2024-09-02 18:21:07 +0530

    GetConnect Build

---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,35 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import Form from "./Form";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          GetConnect
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.primary.contrastText}
+        border= "1px solid black"
+      >
+        
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
